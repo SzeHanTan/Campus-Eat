0921086cf99a3ca59092a3a27cb1a67275ac7374
Hui Yun <huiyun001126@gmail.com>
2024-11-28 17:49:16 +0800

    5th commit

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,6 +1,4 @@
-import axios from 'axios';
-
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState } from 'react';
 
 export const StoreContext = createContext(null);
 
