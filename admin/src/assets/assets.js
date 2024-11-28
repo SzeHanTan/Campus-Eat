de0f5176a3cb41fab2efdf56bebf3673e0787a07
SzeHanTan <147508070+SzeHanTan@users.noreply.github.com>
2024-11-28 22:49:05 +0800

    update for assets, pages and components of admin folder

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,15 @@
+import logo from './logo.png'
+import add_icon from './add_icon_white.png'
+import order_icon from './icon-order.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icom.png'
+
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon
+}
