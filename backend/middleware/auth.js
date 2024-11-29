1c5db12e56068e7c241103df90a5df90d7e34d8e
Your Name <your_email@example.com>
2024-11-29 16:18:11 +0800

    backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,22 +1,19 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const authMiddleware = async (req,res,next) => {
-    const {token} = req.headers;
-    if(!token){
-        return res.json({success:false,message:"Not authorized Login Again"})
+const authMiddleware = async (req, res, next) => {
+    const token = req.headers['token']; 
+    if (!token) {
+        return res.status(401).json({ success: false, message: "Not authorized. Login again" });
     }
-    try{
-        const token_decode = jwt.verify(token,process.env.JWT_SECRET);
-        req.body.userID = token_decode.id;
-        next();
+
+    try {
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET); 
+        req.body.userId = token_decode.id;  
+        next(); 
     } catch (error) {
-        console.log(error);
-        res.json({success:false,message:"Error"})
-
-
+        console.error(error);
+        return res.status(403).json({ success: false, message: "Invalid token. Please login again" });
     }
-
-
-}
+};
 
 export default authMiddleware;
