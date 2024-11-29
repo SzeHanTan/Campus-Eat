a188e51f04d097c34efad2d4ff95927f0d166023
Tyvone.05 <yvonetangyvone@gmail.com>
2024-11-29 14:06:50 +0800

    message

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,31 @@
 import React, { useState, useContext } from 'react';
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext'; 
 
 const Navbar = ({setShowLogin}) => {
 
-    const [menu,setmenu] = useState("home");
+    const [menu,setMenu] = useState("home");
 
-    const{getTotalCartAmount}=useContext(StoreContext);
+    const{getTotalCartAmount,token,setToken}=useContext(StoreContext);
+
+    const navigate = useNavigate();
+
+    const logout = () =>{
+      localStorage.removeItem("token");
+      setToken("");
+      navigate("/")
+    }
 
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <Link to='/'onClick={()=>setmenu("home")} className={menu==="home"?"active":""}>home</Link>
-        <a href='#explore-menu' onClick={()=>setmenu("menu")}className={menu==="menu"?"active":""}>menu</a>
-        <a href='#group-discount' onClick={()=>setmenu("group-discount")}className={menu==="group-discount"?"active":""}>group discount</a>
-        <a href='#footer' onClick={()=>setmenu("contact us")}className={menu==="contact-us"?"active":""}>contact us</a>
+        <Link to='/'onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")}className={menu==="menu"?"active":""}>menu</a>
+        <a href='#group-discount' onClick={()=>setMenu("group-discount")}className={menu==="group-discount"?"active":""}>group discount</a>
+        <a href='#footer' onClick={()=>setMenu("contact us")}className={menu==="contact-us"?"active":""}>contact us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
@@ -25,7 +33,15 @@ const Navbar = ({setShowLogin}) => {
           <img src={assets.basket_icon} alt="" />
           <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
+        {!token?<button onClick={()=>setShowLogin(true)}>sign in</button>
+        :<div className='navbar-profile'>
+            <img src={assets.profile_icon} alt="" />
+            <ul className="nav-profile-dropdown">
+              <li onClick={()=>navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+              <hr />
+              <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+            </ul>
+          </div>}
       </div>
     </div>
   )
